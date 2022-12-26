d9b9ba8ebc4e96fe43071cd3bce5d6f59da23530
Burak Yueksel <mail.burakyuksel@gmail.com>
2022-12-26 15:15:31 +0100

    adding globals and states and etc for phsics. Makefile linking issues

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "physics.h"
+
+
+int main (int argc, char **argv)
+{
+    physicsInit();
+    physicsMain();
+}
--- physics.c
+++ physics.c
@@ -8,39 +8,44 @@
 #include "parameters.h"
 #include "physics.h"
 
-int main() {
-    
-    /*define a particle as a point object*/
-    pointObject particle;
 
-    /*define its mass*/
-    particle.mass_kg    = POINT_MASS_KG;
-    /*define its drag coeff*/
-    particle.dragCoeff  = POINT_DRAGCOEFF;
-    /*define moment of inertia as a 3x3 matrix*/
-    particle.I_kgm2 = newMatrix(3, 3);
-    /*set the moment of inertia terms*/
-    setMatrixElement(particle.I_kgm2, 1, 1, POINT_I_XX_KGM2);//Ixx
-    setMatrixElement(particle.I_kgm2, 2, 2, POINT_I_YY_KGM2);//IYY
-    setMatrixElement(particle.I_kgm2, 3, 3, POINT_I_ZZ_KGM2);//IZZ
-    setMatrixElement(particle.I_kgm2, 1, 2, POINT_I_XY_KGM2);//IXY
-    setMatrixElement(particle.I_kgm2, 2, 1, POINT_I_XY_KGM2);//IYX=IXY
-    setMatrixElement(particle.I_kgm2, 1, 3, POINT_I_XZ_KGM2);//IXZ
-    setMatrixElement(particle.I_kgm2, 3, 1, POINT_I_XZ_KGM2);//IZX=IXZ
-    setMatrixElement(particle.I_kgm2, 2, 3, POINT_I_YZ_KGM2);//IYZ
-    setMatrixElement(particle.I_kgm2, 3, 2, POINT_I_YZ_KGM2);//IZY=IYZ
-    
-    
+/*define a particle as a point object*/
+pointObject g_physicsPointObj;
+
+/** @brief Main  Physics Function */
+void physicsMain()
+{
     // short test
     printf("Mass the Point Particle:\n");
-    printf("% 6.2f ", particle.mass_kg);
+    printf("% 6.2f ", g_physicsPointObj.mass_kg);
     printf("\n");
 
     printf("Drag Coeff of the Point Particle:\n");
-    printf("% 6.2f ", particle.dragCoeff);
+    printf("% 6.2f ", g_physicsPointObj.dragCoeff);
     printf("\n");
 
     printf("Moment of Inertia of the Point Particle:\n");
-    printMatrix(particle.I_kgm2);
+    printMatrix(g_physicsPointObj.I_kgm2);
 
+}
+
+/** @brief Initialize Physics */
+void physicsInit()
+{
+    /*define its mass*/
+    g_physicsPointObj.mass_kg    = POINT_MASS_KG;
+    /*define its drag coeff*/
+    g_physicsPointObj.dragCoeff  = POINT_DRAGCOEFF;
+    /*define moment of inertia as a 3x3 matrix*/
+    g_physicsPointObj.I_kgm2 = newMatrix(3, 3);
+    /*set the moment of inertia terms*/
+    setMatrixElement(g_physicsPointObj.I_kgm2, 1, 1, POINT_I_XX_KGM2);//Ixx
+    setMatrixElement(g_physicsPointObj.I_kgm2, 2, 2, POINT_I_YY_KGM2);//IYY
+    setMatrixElement(g_physicsPointObj.I_kgm2, 3, 3, POINT_I_ZZ_KGM2);//IZZ
+    setMatrixElement(g_physicsPointObj.I_kgm2, 1, 2, POINT_I_XY_KGM2);//IXY
+    setMatrixElement(g_physicsPointObj.I_kgm2, 2, 1, POINT_I_XY_KGM2);//IYX=IXY
+    setMatrixElement(g_physicsPointObj.I_kgm2, 1, 3, POINT_I_XZ_KGM2);//IXZ
+    setMatrixElement(g_physicsPointObj.I_kgm2, 3, 1, POINT_I_XZ_KGM2);//IZX=IXZ
+    setMatrixElement(g_physicsPointObj.I_kgm2, 2, 3, POINT_I_YZ_KGM2);//IYZ
+    setMatrixElement(g_physicsPointObj.I_kgm2, 3, 2, POINT_I_YZ_KGM2);//IZY=IYZ
 }
