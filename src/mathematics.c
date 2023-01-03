6431b40a2b2d37295485f8c531f5b997dd313255
Burak Yueksel <mail.burakyuksel@gmail.com>
2023-01-03 22:19:14 +0100

    add an IMU model with normally distributed noise and fixed bias for each measurement. Improve the mathematics libs

---
+++ src/mathematics.c
@@ -0,0 +1,40 @@
+/**
+ * @author  Burak Yueksel
+ * @date    03 January 2023
+ * @brief   Math and statistics libraries
+ * @addtogroup MATH
+ **/
+
+#include "mathematics.h"
+#include <math.h>       // for sqrt, cos, log, etc
+#include <stdlib.h>     // for RAND_MAX
+
+/** @brief Generate a normally distributed random number*/
+float randn()
+{
+  float u = (float)rand() / (float)RAND_MAX;
+  float v = (float)rand() / (float)RAND_MAX;
+  return sqrt(-2.0 * log(u)) * cos(2.0 * M_PI * v);
+}
+
+/** @brief Compute the approximation square root of a number using Newton's method */
+/* Babylonian method" or "Newton's method" to approximate the square root of a number
+ * Source: https://en.wikipedia.org/wiki/Methods_of_computing_square_roots
+*/
+float newton_sqrt(float x)
+{
+    if (x <= 0) {
+        return 0;
+    }
+    float result = x;
+    float delta;
+    int i;
+    for (i = 0; i < 10; i++) {
+        if (result <= 0) {
+            result = 0.1;
+        }
+        delta = x - (result * result);
+        result = result + 0.5 * delta / result;
+    }
+    return result;
+}
