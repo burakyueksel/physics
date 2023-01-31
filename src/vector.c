bef44c50814361016e1ae3272925cf463d8dcd33
Burak Yueksel <mail.burakyuksel@gmail.com>
2023-01-31 17:28:45 +0100

    add quaternion multiplication and inversion

--- src/vector.c
+++ src/vector.c
@@ -13,3 +13,24 @@ void crossProduct(vector3 a, vector3 b, vector3 axb)
   axb[1] = a[2]*b[0] - a[0]*b[2];
   axb[2] = a[0]*b[1] - a[1]*b[0];
 }
+
+void quaternionProduct(quaternion q1, quaternion q2, quaternion q1xq2)
+{
+  // source: https://www.flyingmachinearena.ethz.ch/wp-content/publications/2018/breTCST18.pdf
+  // equations (2) and (3), explicit implementation
+  // q1xq2 is like R=R1*R2, where R1 and R2 are two rotation matrices, where each corresponds to q1 and q2, respectively.
+  q1xq2.w = q2.w*q1.w - q2.x*q1.x - q2.y*q1.y - q2.z*q1.z;
+  q1xq2.x = q2.w*q1.x + q2.x*q1.w + q2.y*q1.z - q2.z*q1.y;
+  q1xq2.y = q2.w*q1.y - q2.x*q1.z + q2.y*q1.w + q2.z*q1.x;
+  q1xq2.z = q2.w*q1.z + q2.x*q1.y - q2.y*q1.x + q2.z*q1.w;
+}
+
+void quaternionInverse(quaternion q, quaternion q_inverse)
+  // for q is the quaternion representing orientation from frame F1 to frame F2,
+  // q_inverse is the quaternion that represents the orientation from F2 to frame F1.
+{
+  q_inverse.w = q.w;
+  q_inverse.x = -q.x;
+  q_inverse.y = -q.y;
+  q_inverse.z = -q.z;
+}
